3db9c40b6df27189b799667bbb28b254e465ca38
NabeelK <xnabeeelo@hotmail.com>
2022-07-03 11:40:03 +0300

    Creation commit

---
+++ src/test/java/com/example/MachineService/MachineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MachineService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
